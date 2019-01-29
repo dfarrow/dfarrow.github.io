b6a46f6b8041cc41b03697f259c6e0c309bc9887
Daniel Farrow <daniel_farrow@yahoo.com>
2019-01-29 10:40:34 -0500

    Added JS Function homework for Jan 31.

---
+++ js/numbers.js
@@ -0,0 +1,25 @@
+
+var myNum = 420;
+
+
+console.log("my number is ", myNum);
+
+var cont = document.getElementById("content").textContent =  "MY NUMBER IS " + myNum;
+
+var testAbc = "hello";
+
+var sayText = function(value) {
+    console.log(value);
+}
+
+
+
+sayText("bunny");
+
+var newNum = 120;
+if(newNum > 100) {
+    console.log("WOW BIG!")
+} else {
+    console.log("small");
+}
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-// Nothing is here yet...
+// Function for jan31.html 
+
+var wasRun = false;
+
+function start() {
+
+    // Get a reference to the triangle
+    var elTriangle = $("#yellowTriangle"); 
+        
+    if(!wasRun) {
+        wasRun = true;
+        var elTriangle = $("#yellowTriangle"); 
+        
+        // Animate the SVG properties
+        $("#yellowTriangle").animate({svgWidth: 200, svgHeight: '30%', 
+            svgStroke: 'aqua', 
+            svgStrokeWidth: '+=7', 
+            svgFill: 'red',
+            svgTransform: 'translate(400, 100) rotate(360, 50, 50)'}, 2000);
+    } else {
+        console.log("You already ran this function!"); 
+        wasRun = false; 
+        // Return the SVG properties to initial state
+        $("#yellowTriangle").animate({svgWidth: 200, svgHeight: '30%', 
+            svgStroke: 'aqua', 
+            svgStrokeWidth: '0', 
+            svgFill: 'lemonchiffon',
+            svgTransform: 'translate(10, 100) rotate(0, 50, 50)'}, 2000);
+        
+    }
+ 
+}
